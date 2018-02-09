9ad02e4465f752cbb787f42574735dea660344ad
talidemestre <30426066+talidemestre@users.noreply.github.com>
2018-02-09 18:39:56 +1100

    added code comments

--- calendar.py
+++ calendar.py
@@ -1,31 +1,20 @@
 years=[]
+#creates intial year array
 for i in range(2018, 2028):
     years.append([])    
 year = 2018
+#populates the years with days and months
 for x in years:    
-    for i in range(0,12):
+    for i in range(0,12):        
         if (i != 8) and (i != 3) and (i != 5) and (i != 9)and (i != 1):
             x.append(['','','','','','','','','','','','','','','','','','','','','','','','','','','','','','',''])
         elif (i == 1):
+            #checks if the year is a leap year
             if (year%400==0) or (year%4 == 0) and (year%100!=0):
                 x.append(['','','','','','','','','','','','','','','','','','','','','','','','','','','','',''])
             else:
                 x.append(['','','','','','','','','','','','','','','','','','','','','','','','','','','',''])
         else:
             x.append(['','','','','','','','','','','','','','','','','','','','','','','','','','','','','','',''])
+    #increases year count for leap year checking
     year+=1
-
-
-
-january = []
-februray = []
-march = []
-april=[]
-may = []
-june =[]
-july = []
-august = []
-september = []
-october=[]
-november=[]
-december=[] 
