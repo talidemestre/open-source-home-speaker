1f875e73366dc711f0dfa5381325f71f73adab0e
talidemestre <30426066+talidemestre@users.noreply.github.com>
2018-02-10 11:13:00 +1100

    splitting files -NOT WORKING

---
+++ generate_date.py
@@ -0,0 +1,23 @@
+import time
+currentYear = int(time.strftime("%Y"))
+years=[]
+#creates intial year array
+for i in range(currentYear, currentYear+10):
+    years.append([])    
+year = currentYear
+#populates the years with days and months
+for x in years:    
+    for i in range(0,12):        
+        if (i != 8) and (i != 3) and (i != 5) and (i != 9)and (i != 1):
+            x.append(['','','','','','','','','','','','','','','','','','','','','','','','','','','','','','',''])
+        elif (i == 1):
+            #checks if the year is a leap year
+            if (year%400==0) or (year%4 == 0) and (year%100!=0):
+                x.append(['','','','','','','','','','','','','','','','','','','','','','','','','','','','',''])
+            else:
+                x.append(['','','','','','','','','','','','','','','','','','','','','','','','','','','',''])
+        else:
+            x.append(['','','','','','','','','','','','','','','','','','','','','','','','','','','','','','',''])
+    #increases year count for leap year checking
+    year+=1
+
