62dc2a98f4c01f5683f2700e7a7a013487970079
talidemestre <30426066+talidemestre@users.noreply.github.com>
2018-02-10 11:10:00 +1100

    a

--- date.py
+++ date.py
@@ -23,13 +23,12 @@ for x in years:
 
 
 
-testDate = "set the date feed the baby for twenty third march twenty eighteen"
+testDate = "set the date feed the baby for sixteenth november two thousand nineteen"
 testDate = testDate.split(' ')
-FinalMonth = int(time.strftime("%M"))
-FinalDay=32
-FinalYear=int(time.strftime("%Y"))
-
-print (testDate)
+finalMonth = int(time.strftime("%M"))
+finalDay=32
+finalYear=int(time.strftime("%Y"))
+#print (testDate)
 
 dayList = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth', 'ninth', 'tenth', 'eleventh', 'twelfth', 'thirteenth', 'fourteenth', 'fifteenth', 'sixteenth', 'seventeenth', 'eighteenth', 'nineteenth', 'twentieth']
 monthList = ['january',  'february', 'march', 'april', 'may', 'june', 'july', 'august','september', 'october','november','december']
@@ -38,24 +37,26 @@ prefix =['twenty', 'two']
 midfix ='thousand'
 daySuffix = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth', 'ninth']
 yearSuffix = [ 'eighteenth', 'nineteenth', 'twenty']
-
-
 for word in range(0, len(testDate)):
     for month in range(0, len(monthList)):
         if testDate[word] == monthList[month]:
-            FinalMonth = month
+            finalMonth = month
     for day in range(0, len(dayList)):
         if testDate[word] == dayList[day]:
-            FinalDay = day
+            finalDay = day
     if testDate[word] == 'twenty':
         for y in range(0, len(yearSuffix)):
             if testDate[word+1] == yearSuffix[y]:
-                FinalYear == yearSuffix[y] + 2018
+                finalYear == yearSuffix[y] + 2018
         for z in range(0, len(daySuffix)):
             if testDate[word+1] == daySuffix[z]:
-                FinalDay= z + 20
+                finalDay= z + 20
     if testDate[word] == 'two':
         if testDate[word+1] == 'thousand':
             for y in range(0, len(yearSuffix)):
                 if testDate[word+1] == yearSuffix[y]:
-                    FinalYear == yearSuffix[y] + 2018
+                    finalYear == yearSuffix[y] + 2018
+
+print(finalDay, finalMonth, finalYear)
+years[finalYear - int(time.strftime("%Y"))][finalMonth][finalDay] = testDate
+    
