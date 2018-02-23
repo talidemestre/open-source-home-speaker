16156279e7b50e2095b913a4cf87c86368b45e3e
talidemestre <30426066+talidemestre@users.noreply.github.com>
2018-02-24 10:32:40 +1100

    reintegrated date function into master file
    as of this the calendar is stored in its own external .py file and is written over every time a change is made to its contnets. additionally the setup file will reset it entirely.

--- archive files/date.py
+++ archive files/date.py
@@ -1,35 +1,14 @@
 import time
-currentYear = int(time.strftime("%Y"))
-years=[]
-#creates intial year array
-for i in range(currentYear, currentYear+10):
-    years.append([])    
-year = currentYear
-#populates the years with days and months
-for x in years:    
-    for i in range(0,12):        
-        if (i != 8) and (i != 3) and (i != 5) and (i != 9)and (i != 1):
-            x.append(['','','','','','','','','','','','','','','','','','','','','','','','','','','','','','',''])
-        elif (i == 1):
-            #checks if the year is a leap year
-            if (year%400==0) or (year%4 == 0) and (year%100!=0):
-                x.append(['','','','','','','','','','','','','','','','','','','','','','','','','','','','',''])
-            else:
-                x.append(['','','','','','','','','','','','','','','','','','','','','','','','','','','',''])
-        else:
-            x.append(['','','','','','','','','','','','','','','','','','','','','','','','','','','','','','',''])
-    #increases year count for leap year checking
-    year+=1
+import import_calendar
+
+UserVoiceInput = "set an evetn on the first of january two thousand eighteen"
 
 
-
-testDate = "set the date feed the baby for twenty third march twenty eighteen"
-testDate = testDate.split(' ')
-FinalMonth = 12
-FinalDay=32
-FinalYear=2000
-
-print (testDate)
+testDate = UserVoiceInput.split(' ')
+finalMonth = int(time.strftime("%M"))
+finalDay=32
+finalYear=int(time.strftime("%Y"))
+#print (testDate)
 
 dayList = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth', 'ninth', 'tenth', 'eleventh', 'twelfth', 'thirteenth', 'fourteenth', 'fifteenth', 'sixteenth', 'seventeenth', 'eighteenth', 'nineteenth', 'twentieth']
 monthList = ['january',  'february', 'march', 'april', 'may', 'june', 'july', 'august','september', 'october','november','december']
@@ -37,15 +16,30 @@ monthList = ['january',  'february', 'march', 'april', 'may', 'june', 'july', 'a
 prefix =['twenty', 'two']
 midfix ='thousand'
 daySuffix = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth', 'ninth']
-yearSuffix = [ 'eighteenth', 'nineteenth', 'twenty']
-
-
+yearSuffix = [ 'eighteen', 'nineteen', 'twenty']
 for word in range(0, len(testDate)):
     for month in range(0, len(monthList)):
         if testDate[word] == monthList[month]:
-            FinalMonth = month
+            finalMonth = month
     for day in range(0, len(dayList)):
-        if testDate[word] == dayList[day]:
-            FinalDay = day
-    for 
-    
+        if testDate[word] == dayList[day] and testDate[word-1] != 'twenty' and testDate[word-1] != 'thousand':
+            finalDay = day
+    if testDate[word] == 'twenty':
+        for y in range(0, len(yearSuffix)):
+            if testDate[word+1] == yearSuffix[y]:
+                finalYear == yearSuffix[y] + 2018
+        for z in range(0, len(daySuffix)):
+            if testDate[word+1] == daySuffix[z]:
+                finalDay= z + 20
+    if testDate[word] == 'two':
+        if testDate[word+1] == 'thousand':
+            for y in range(0, len(yearSuffix)):
+                if testDate[word+1] == yearSuffix[y]:
+                    finalYear == yearSuffix[y] + 2018
+
+print(finalDay, finalMonth, finalYear)
+import_calendar.years[finalYear - int(time.strftime("%Y"))][finalMonth][finalDay] = UserVoiceInput
+write_data = open('import_calendar.py', 'w')
+write_data.write("years =" + str(import_calendar.years))
+write_data.close()
+
--- master.py
+++ master.py
@@ -1,4 +1,5 @@
 import pypygo
+import time
 import import_calendar
 
 #UserVoiceInput = "Hey google, set an alarm for four thirty"
@@ -32,7 +33,45 @@ def Command1(UserVoiceInput):
 def Command2(UserVoiceInput):
         print ("Attempting to set alarm...")
 def Command3(UserVoiceInput):
-        print("Accessing calendar...")
+        testDate = UserVoiceInput.split(' ')
+        finalMonth = int(time.strftime("%M"))
+        finalDay=32
+        finalYear=int(time.strftime("%Y"))
+        #print (testDate)
+
+        dayList = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth', 'ninth', 'tenth', 'eleventh', 'twelfth', 'thirteenth', 'fourteenth', 'fifteenth', 'sixteenth', 'seventeenth', 'eighteenth', 'nineteenth', 'twentieth']
+        monthList = ['january',  'february', 'march', 'april', 'may', 'june', 'july', 'august','september', 'october','november','december']
+
+        prefix =['twenty', 'two']
+        midfix ='thousand'
+        daySuffix = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth', 'ninth']
+        yearSuffix = [ 'eighteen', 'nineteen', 'twenty']
+        for word in range(0, len(testDate)):
+            for month in range(0, len(monthList)):
+                if testDate[word] == monthList[month]:
+                    finalMonth = month
+            for day in range(0, len(dayList)):
+                if testDate[word] == dayList[day] and testDate[word-1] != 'twenty' and testDate[word-1] != 'thousand':
+                    finalDay = day
+            if testDate[word] == 'twenty':
+                for y in range(0, len(yearSuffix)):
+                    if testDate[word+1] == yearSuffix[y]:
+                        finalYear == yearSuffix[y] + 2018
+                for z in range(0, len(daySuffix)):
+                    if testDate[word+1] == daySuffix[z]:
+                        finalDay= z + 20
+            if testDate[word] == 'two':
+                if testDate[word+1] == 'thousand':
+                    for y in range(0, len(yearSuffix)):
+                        if testDate[word+1] == yearSuffix[y]:
+                            finalYear == yearSuffix[y] + 2018
+
+        print(finalDay, finalMonth, finalYear)
+        import_calendar.years[finalYear - int(time.strftime("%Y"))][finalMonth][finalDay] = UserVoiceInput
+        write_data = open('import_calendar.py', 'w')
+        write_data.write("years =" + str(import_calendar.years))
+        write_data.close()
+        
 def Command4(UserVoiceInput):
         print ("Attempting to gather schedule...")
 def Command5(UserVoiceInput):
