831d57dfcff36aee496b0fc0e978acc3a5432da3
talidemestre <30426066+talidemestre@users.noreply.github.com>
2018-02-24 12:10:58 +1100

    retrieve events of next 7 days
    includes the abillity to go across months

--- gather_schedule.py
+++ gather_schedule.py
@@ -12,7 +12,17 @@ arrayDay = currentDay - 1
 
 day_range = 7
 
-for i in range(arrayDay, arrayDay+7):
-    print( str(i+1))
-    print(import_calendar.years[arrayYear][arrayMonth][i])
-    
+if (day_range + arrayDay <= len(import_calendar.years[arrayYear][arrayMonth])):
+    for i in range(arrayDay, arrayDay+day_range):
+        print( str(arrayMonth) +" " +  str(i+1))
+        print(import_calendar.years[arrayYear][arrayMonth][i])
+else:
+    for i in range(arrayDay, arrayDay + (len(import_calendar.years[arrayYear][arrayMonth]) - arrayDay)):
+        print( str(arrayMonth) +" " +  str(i+1))
+        print(import_calendar.years[arrayYear][arrayMonth][i])
+
+
+        
+    for i in range(0, day_range - (len(import_calendar.years[arrayYear][arrayMonth]) - arrayDay)):
+        print( str(arrayMonth+1) +" " +  str(i+1))
+        print(import_calendar.years[arrayYear][arrayMonth + 1][i])
