dd62c9ac4b82370cbf78176b2e551cae6d25d5e8
talidemestre <30426066+talidemestre@users.noreply.github.com>
2018-04-26 12:50:18 +1000

    streamlined
    simplified and added AM vs PM detection

--- alarm_temp.py
+++ alarm_temp.py
@@ -12,59 +12,30 @@ minuteSuffix =['one', 'two', 'three', 'four', 'five', "six", "seven", "eight", "
 
 
 
-
-
-hour_word = ""
-minute_mono_word = ""
-minute_prefix_word = ""
-minute_suffix_word = ""
-
-
+finalHour=0
+finalMinute=0
 AMorPM = "PM" ###DEFAULT PLEASE CHANGE###
-
-
-
 hour_detected = False
+
 for word in range(0, len(VoiceArray)):
     print (VoiceArray[word])
+    if VoiceArray[word] == 'AM':
+        AMorPM ="AM"
+    if VoiceArray[word] == 'PM':
+        AMorPM ="PM"
+                
     if hour_detected == False:
         for num in range (0, len(hourList)):
             if VoiceArray[word] == hourList[num]:
                 hour_detected = True
-                hourFoundAt = word
-                hour_word = VoiceArray[word]               
+                finalHour = word+1              
     else:
-        for num in range (0, len(minuteMono)):
-            if VoiceArray[word] == minuteMono[num]:
-                minute_mono_word = VoiceArray[word]
-        for num in range (0, len(minutePrefix)):
-            if VoiceArray[word] == minutePrefix[num]:
-                minute_prefix_word = VoiceArray[word]
-        for num in range (0, len(minuteSuffix)):
-            if VoiceArray[word] == minuteSuffix[num]:
-                minute_suffix_word = VoiceArray[word]
-                
-##Converting Words into Numbers##
-finalHour=0
-finalMinute=0
-
-for i in range (0, len(hourList)):
-    if hour_word == hourList[i]:
-        finalHour = i+1
-
-if minute_mono_word == '':
-    if minute_prefix_word != '':
         for i in range (0, len(minutePrefix)):
-            if minute_prefix_word == minutePrefix[i]:
+            if VoiceArray[word] == minutePrefix[i]:
                 finalMinute+= 10*((i+2))
-                                  
-    if minute_suffix_word != '':                                  
         for i in range (0, len(minuteSuffix)):
-            if minute_suffix_word == minuteSuffix[i]:
+            if VoiceArray[word] == minuteSuffix[i]:
                 finalMinute += i+1
-else:
-    for i in range (0, len(minuteMono)):
-        if minute_mono_word == minuteMono[i]:
-            finalMinute = i + 10
-    
-    
+        for i in range (0, len(minuteMono)):
+            if VoiceArray[word] == minuteMono[i]:
+                finalMinute = i + 10
