2173b434b4f1c0d59361753d7bbb9bcf9ddabcea
Tali <30426066+talidemestre@users.noreply.github.com>
2018-07-04 23:45:22 +1000

    shell script

---
+++ run_tommy.sh
@@ -0,0 +1,13 @@
+xdotool key ctrl+alt+t
+xdotool key --delay 1000 Return
+xdotool type 'cd Documents/open-source-home-speaker'
+xdotool key Return
+xdotool type 'python3 ring_alarm.py'
+xdotool key Return
+xdotool key --delay 500 Return
+xdotool key ctrl+alt+t
+xdotool key --delay 500 Return
+xdotool type 'cd Documents/open-source-home-speaker'
+xdotool key Return
+xdotool type 'python3 master.py'
+xdotool key Return
