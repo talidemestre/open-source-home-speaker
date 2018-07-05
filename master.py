16b8f4503c4911ffdd64f8a9c89281d36b5ac0ba
Tali <30426066+talidemestre@users.noreply.github.com>
2018-07-05 17:08:10 +1000

    up to 2020 on calendar

--- master.py
+++ master.py
@@ -176,70 +176,73 @@ def Command2(UserVoiceInput):
                 DisplayAndSay("Alarm set for " + str(finalHour) + " " + str(finalMinute) + " " + str(AMorPM))
 #COMMAND THAT SETS DATES        
 def Command3(UserVoiceInput):
-        importlib.reload(import_calendar)
+        try:
+                importlib.reload(import_calendar)
 
-        testDate = UserVoiceInput.split(' ')
-        finalMonth = int(time.strftime("%m"))
-        finalDay=int(time.strftime("%d"))+1
-        finalYear=int(time.strftime("%Y"))
+                testDate = UserVoiceInput.split(' ')
+                finalMonth = int(time.strftime("%m"))
+                finalDay=int(time.strftime("%d"))+1
+                finalYear=int(time.strftime("%Y"))
 
-        VoiceArray = UserVoiceInput.split(" ")
-        
-        ###ADD THIRTIETH
-        daySpecialCase ="thirtieth"
+                VoiceArray = UserVoiceInput.split(" ")
+                
+                ###ADD THIRTIETH
+                daySpecialCase ="thirtieth"
 
-        dayList = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth', 'ninth', 'tenth', 'eleventh', 'twelfth', 'thirteenth', 'fourteenth', 'fifteenth', 'sixteenth', 'seventeenth', 'eighteenth', 'nineteenth', 'twentieth']
-        monthList = ['january',  'february', 'march', 'april', 'may', 'june', 'july', 'august','september', 'october','november','december']
-
-
+                dayList = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth', 'ninth', 'tenth', 'eleventh', 'twelfth', 'thirteenth', 'fourteenth', 'fifteenth', 'sixteenth', 'seventeenth', 'eighteenth', 'nineteenth', 'twentieth']
+                monthList = ['january',  'february', 'march', 'april', 'may', 'june', 'july', 'august','september', 'october','november','december']
 
 
-        #prefixes/suffixes to be checked to determine the day/date using an iterable string
-        prefix =['twenty', 'two']
-        midfix ='thousand'
 
-        daySuffix = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth', 'ninth']
-        yearSuffix = [ 'eighteen', 'nineteen', 'twenty']
 
-        #iterating through the string and matching against words in the previous strings to determine the requested date
-        for word in range(0, len(testDate)):
-            for month in range(0, len(monthList)):
-                if testDate[word] == monthList[month]:
-                    finalMonth = month
-            for day in range(0, len(dayList)):
-                if testDate[word] == dayList[day] and testDate[word-1] != 'twenty' and testDate[word-1] != 'thirty' and testDate[word-1] != 'thousand':
-                    finalDay = day
-            if testDate[word] == 'thirtieth':
-                finalDay = 29
-            if testDate[word] == 'twenty':
-                for y in range(0, len(yearSuffix)):
-                    if testDate[word+1] == yearSuffix[y]:
-                        finalYear == yearSuffix[y] + import_calendar.creationYear
-                for z in range(0, len(daySuffix)):
-                    if testDate[word+1] == daySuffix[z]:
-                        finalDay= z + 20
-            if testDate[word] == 'thirty':
-                print("found")
-                for y in range(0, len(yearSuffix)):
-                    if testDate[word+1] == yearSuffix[y]:
-                        finalYear == yearSuffix[y] + import_calendar.creationYear
-                for z in range(0, len(daySuffix)):
-                    if testDate[word+1] == daySuffix[z]:
-                        print("made it")
-                        finalDay= z + 30
-            if testDate[word] == 'two':
-                if testDate[word+1] == 'thousand':
-                    for y in range(0, len(yearSuffix)):
-                        if testDate[word+1] == yearSuffix[y]:
-                            finalYear == yearSuffix[y] + import_calendar.creationYear
+                #prefixes/suffixes to be checked to determine the day/date using an iterable string
+                prefix =['twenty', 'two']
+                midfix ='thousand'
 
-        #print(finalDay, finalMonth, finalYear)
-        #writing results to the file that stores the calendar
-        import_calendar.years[finalYear - int(time.strftime("%Y"))][finalMonth][finalDay] = UserVoiceInput
-        write_data = open('import_calendar.py', 'w')
-        write_data.write("creationYear=" + str(import_calendar.creationYear) + "\nyears =" + str(import_calendar.years))
-        write_data.close()
-        DisplayAndSay("Event set for " + str(finalDay+1) + " " + monthList[finalMonth] + " " + str(finalYear))
+                daySuffix = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth', 'ninth']
+                yearSuffix = [ 'eighteen', 'nineteen', 'twenty']
+
+                #iterating through the string and matching against words in the previous strings to determine the requested date
+                for word in range(0, len(testDate)):
+                    for month in range(0, len(monthList)):
+                        if testDate[word] == monthList[month]:
+                            finalMonth = month
+                    for day in range(0, len(dayList)):
+                        if testDate[word] == dayList[day] and testDate[word-1] != 'twenty' and testDate[word-1] != 'thirty' and testDate[word-1] != 'thousand':
+                            finalDay = day
+                    if testDate[word] == 'thirtieth':
+                        finalDay = 29
+                    if testDate[word] == 'twenty':
+                        for y in range(0, len(yearSuffix)):
+                            if testDate[word+1] == yearSuffix[y]:
+                                finalYear = y + import_calendar.creationYear
+                        for z in range(0, len(daySuffix)):
+                            if testDate[word+1] == daySuffix[z]:
+                                finalDay= z + 20
+                    if testDate[word] == 'thirty':
+                        print("found")
+                        for y in range(0, len(yearSuffix)):
+                            if testDate[word+1] == yearSuffix[y]:
+                                finalYear = y + import_calendar.creationYear
+                        for z in range(0, len(daySuffix)):
+                            if testDate[word+1] == daySuffix[z]:
+                                print("made it")
+                                finalDay= z + 30
+                    if testDate[word] == 'two':
+                        if testDate[word+1] == 'thousand':
+                            for y in range(0, len(yearSuffix)):
+                                if testDate[word+1] == yearSuffix[y]:
+                                    finalYear = y + import_calendar.creationYear
+
+                #print(finalDay, finalMonth, finalYear)
+                #writing results to the file that stores the calendar
+                import_calendar.years[finalYear - int(time.strftime("%Y"))][finalMonth][finalDay] = UserVoiceInput
+                write_data = open('import_calendar.py', 'w')
+                write_data.write("creationYear=" + str(import_calendar.creationYear) + "\nyears =" + str(import_calendar.years))
+                write_data.close()
+                DisplayAndSay("Event set for " + str(finalDay+1) + " " + monthList[finalMonth] + " " + str(finalYear))
+        except:
+                 DisplayAndSay("Something went wrong in detecting the date. Please try again.")
 
 ##COMMAND THAT GATHERS EVENTS ON CALENDAR        
 def Command4(UserVoiceInput):
